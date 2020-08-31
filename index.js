3c6757f2193913920237e425b53e1f8bdab964b8
mduduziavatar <mduduziavatar@gmail.com>
2020-08-31 16:12:50 +0200

    saving progress

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ const PORT = process.env.PORT || 3008;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({ layoutsDir: './views/layouts' }));
-
 app.use(express.static('public'));
+// app flash setups 
+var flash = require('express-flash');
+var session = require('express-session')
+
+// app.use(express.cookieParser('keyboard cat'));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+app.use(flash());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -21,19 +31,24 @@ app.get('/', function(req, res) {
 });
 
 app.post("/greet", function(req, res) {
+
     const item = req.body.textitem
     const language = req.body.selector
-    const greetedUsers = greetFactory.greetUser(item, language)
 
-    greetFactory.greetUser(
-        req.body.textitem
-    );
 
-    greetFactory.greetUser(req.body.textitem)
-    console.log(req.body);
-    // note that data can be sent to the template
+    if (item == "") {
+        req.flash('index', 'please enter');
+    }
+
+
+    // const greetedUsers = greetFactory.greetUser(item, language)
+    // greetFactory.greetUser(
+    //     req.body.textitem
+    // )
+    // console.log(req.body);
+    // // note that data can be sent to the template
     res.render('index', {
-        txtBox: greetedUsers
+
     });
 });
 
