81cdee94e928f306259ead6bc8bf4bffb14a96ee
mduduziavatar <mduduziavatar@gmail.com>
2020-10-12 15:52:21 +0200

    fixing local changes

--- index.js
+++ index.js
@@ -24,20 +24,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 //sending back home
-app.get('/', async function (req, res) {
+app.get('/', async function(req, res) {
     res.render('index');
 });
 
 //greet app setup
-app.post("/greet", async function (req, res) {
+app.post("/greet", async function(req, res) {
     const name = req.body.textItem;
     const language = req.body.selector;
-<<<<<<< HEAD
-    const greetedUsers = await greetFactory.greetUser(name, language);
-
-=======
     const greetedUsers = greetFactory.greetUser(name, language);
->>>>>>> e97ad8e968da7b7d3227e1be3290e30131263b03
     if (name === "" && language === undefined) {
         req.flash("errors", "please enter a name and select a language ");
     } else if (name === "") {
@@ -58,7 +53,7 @@ app.post("/greet", async function (req, res) {
     });
 });
 
-app.get('/data', async function (req, res) {
+app.get('/data', async function(req, res) {
     var name = req.params.name;
 
     var data = {
@@ -68,7 +63,7 @@ app.get('/data', async function (req, res) {
     res.render('data', data);
 });
 
-app.get('/counter/:name', async function (req, res) {
+app.get('/counter/:name', async function(req, res) {
     var name = req.params.name;
     var count = await greetFactory.perPerson(name)
     res.render('counter', {
@@ -77,17 +72,15 @@ app.get('/counter/:name', async function (req, res) {
     })
 });
 
-app.get('/reset', async function (req, res) {
+app.get('/reset', async function(req, res) {
     var reset = await greetFactory.reset()
     res.render('index')
 });
 
-<<<<<<< HEAD
+
 const PORT = process.env.PORT || 3008;
 
+
 app.listen(PORT, function() {
-=======
-app.listen(PORT, function () {
->>>>>>> e97ad8e968da7b7d3227e1be3290e30131263b03
     console.log('App starting on port', PORT);
 });
